95c373a761adf8977d8ab11561169450691a7570
emilyconnearney2 <90657767+emilyconnearney2@users.noreply.github.com>
2022-01-09 17:43:28 -0500

    end of section6

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
-import { AccountService } from '../_services/account.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,25 +11,25 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, 
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
-
   }
 
-  login(){
+  login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
-  logout(){
+  logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
-
 }
