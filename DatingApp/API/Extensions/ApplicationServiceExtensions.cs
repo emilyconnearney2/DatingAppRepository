8bc814c95c9095554e4433f646677b6afe4e7cfa
emilyconnearney2 <90657767+emilyconnearney2@users.noreply.github.com>
2022-01-10 11:48:06 -0500

    end of section9: fixed

--- DatingApp/API/Extensions/ApplicationServiceExtensions.cs
+++ DatingApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -20,6 +20,7 @@ namespace API.Extensions
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+
             return services;
         }
     }
